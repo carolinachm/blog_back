53cc19e4d3386715d9cc22015b86d57714cebeb1
Carolina Mesquita <carolti2013@gmail.com>
2018-07-10 23:15:36 -0300

    create funcionando

--- controllers/galeria-controller.js
+++ controllers/galeria-controller.js
@@ -3,6 +3,7 @@
 const mongoose = require('mongoose');
 const multer = require('multer');
 
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './uploads/');
@@ -31,43 +32,63 @@ const upload = multer({
 
 var model;
 
-class GaleriaController{
+class GaleriaController {
 
-    constructor(app){
+    constructor(app) {
 
         app.get('/galerias', this.findAll);
-        app.post('/galerias', upload.single('foto'),this.create);
-        app.put('/galerias', this.update);
-        app.delete('/galerias', this.remove);
-        //this model recebe a coleção do banco de dados
+        app.post('/galerias', upload.single('foto'), this.create);
+        app.put('/galerias/:_id', this.update);
+        app.delete('/galerias/:_id', this.delete);
+
         model = mongoose.model('Galeria');
 
-    }
-    async findAll(req, res){
-        res.json (await model.find({}));
-    }
-    async create(req, res){
-      try{
-        let galeria = req.body;
-        galeria.foto = req.file.path;
-        const gal = await model.create(galeria);
-        res.status(201).send({message: "Galeria cadastrado com sucesso",gal});
-      }catch (e) {
-        console.log(e);
-        res.status(500).send({
-            message: 'Falha ao processar sua requisição'
-        });
-    }
 
     }
-    async update(req, res){
-        let galeria = req.body;
-       
-        res.json (await model.findOneAndUpdate({_id:galeria._id}, galeria));
+    async findAll(req, res) {
+        res.json(await model.find({}));
     }
-    async remove(req, res){
-        let id = req.body._id;
-        res.json (await model.remove({_id, id}));
+    async create(req, res) {
+        let galeria = req.body;
+        galeria.foto = req.file.path;
+        try {
+            const gal = await model.create(galeria);
+            res.status(201).send({ message: "Galeria cadastrado com sucesso", gal });
+        } catch (e) {
+            console.log(e);
+            res.status(500).send({
+                message: 'Falha ao processar sua requisição'
+            });
+        }
+
+
+
+    }
+    async update(req, res) {
+        try {
+            await model.update(req.params.id, req.body);
+            res.status(200).send({
+                message: 'Galeria atualizado com sucesso!'
+            });
+        } catch (e) {
+            res.status(500).send({
+                message: 'Falha ao processar sua requisição'
+            });
+        }
+
+    }
+    async delete(req, res) {
+        try {
+            await model.delete(req.body.id)
+            res.status(200).send({
+                message: 'Galeria removido com sucesso!'
+            });
+        } catch (e) {
+            res.status(500).send({
+                message: 'Falha ao processar sua requisição'
+            });
+        }
+
     }
 }
 module.exports = GaleriaController;
