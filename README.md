8e05e83f33985ee00c359fc5486833a5e0615ccd
carolinachm <carolti2013@gmail.com>
2020-12-01 15:17:16 -0300

    Criação do projeto do blog com nodejs e react

--- README.md
+++
@@ -1,3 +0,0 @@
-Descomente o codigo 
-const upload = multer({ dest: 'uploads/' });
-e comente a linha 26
--- config/database.config.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-module.exports = {
-    url: 'mongodb://mongo:mongo12@ds151530.mlab.com:51530/db_blog'
-}
